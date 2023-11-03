6ebc294707b5054d865b407015ecfdab605e25ed
Ondřej Maxa <ondrej.maxa11@gmail.com>
2023-11-03 22:46:15 +0000

    📝 add more examples
    Signed-off-by: Ondřej Maxa <ondrej.maxa11@gmail.com>

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,4 +1,5 @@
-import { console, either, option, taskEither } from 'fp-ts';
+import { dateToStr, subYears } from '$lib/date';
+import { console, date, either, io, option, taskEither } from 'fp-ts';
 import { pipe } from 'fp-ts/lib/function';
 import { DeploymentUnitsService, DeploymentsService, OpenAPI, StartDeploymentRequest } from './client';
 
@@ -26,8 +27,8 @@ export const example = async () => {
 export const getUnits = pipe(
   () =>
     DeploymentUnitsService.list4({
-      createdAtGte: option.some('2023-09-01T20:52:14'),
-      createdAtLte: option.some('2023-11-01T20:52:15'),
+      createdAtGte: pipe(date.create, subYears(1), io.map(dateToStr), (io) => io(), option.some),
+      createdAtLte: pipe(date.create, io.map(dateToStr), (io) => io(), option.some),
     }),
   taskEither.map((r) => r.page),
   taskEither.tapIO((data) => console.log(data)),
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,20 @@
 <script lang="ts">
   import { getUnits } from '$lib';
+  import { either } from 'fp-ts';
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 <button on:click={getUnits}>Run example (check networks tab)</button>
+
+{#await getUnits()}
+  Loading...
+{:then units}
+  {#if either.isLeft(units)}
+    Error: {units.left.message}
+  {:else}
+    {#each units.right as unit}
+      <p>{unit.id}: {unit.name}</p>
+    {/each}
+  {/if}
+{/await}
