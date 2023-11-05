ea183a9fff09fae51fdf87d6a7ccc7926c217de3
Martin Rosenberg <nitram.rosenberg@gmail.com>
2023-11-05 18:43:50 +0000

    🐛 First app module selected

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,7 @@
   import '../app.css';
 
   import { getAppModulesBySas, getSASes } from '$lib';
+  import type { AppModuleResponse } from '$lib/client';
   import { classNames, humanize } from '$lib/string';
   import { selectOptions } from '$lib/util';
   import { either, option } from 'fp-ts';
@@ -11,8 +12,19 @@
   import Sidebar from '../components/layout/Sidebar.svelte';
   import { selectedAppModule } from '../stores';
 
+  let appModules: AppModuleResponse[] = [];
+
   let sasIdInput: [string, string] | null = null;
   $: sasId = sasIdInput === null ? option.none : option.some(sasIdInput);
+
+  $: if (option.isSome(sasId)) {
+    getAppModulesBySas(sasId.value[0])().then((resp) => {
+      if (either.isRight(resp)) {
+        appModules = [...resp.right];
+        selectedAppModule.set(resp.right[0].id);
+      }
+    });
+  }
 
   let selectedAppModuleId: string | null = null;
   selectedAppModule.subscribe((id) => {
@@ -39,37 +51,29 @@
     {/await}
 
     {#if option.isSome(sasId)}
-      {#await getAppModulesBySas(sasId.value[0])()}
-        <Loader />
-      {:then appModules}
-        {#if either.isLeft(appModules)}
-          very error help
-        {:else}
-          <li>
-            <div class="text-xs font-semibold leading-6 text-csas-100">App modules</div>
-            <ul role="list" class="-mx-2 mt-2 space-y-1">
-              {#each appModules.right as module (module.id)}
-                <li>
-                  <a
-                    href="#"
-                    class={classNames(
-                      selectedAppModuleId === module.id
-                        ? 'bg-csas-700 text-white'
-                        : 'text-csas-100 hover:text-white hover:bg-csas-700',
-                      'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold'
-                    )}
-                    on:click={() => {
-                      if (selectedAppModuleId !== module.id) selectedAppModule.set(module.id);
-                    }}
-                  >
-                    <span class="truncate">{humanize(module.name)}</span>
-                  </a>
-                </li>
-              {/each}
-            </ul>
-          </li>
-        {/if}
-      {/await}
+      <li>
+        <div class="text-xs font-semibold leading-6 text-csas-100 uppercase">App modules</div>
+        <ul role="list" class="-mx-2 mt-2 space-y-1">
+          {#each appModules as module (module.id)}
+            <li>
+              <a
+                href="#"
+                class={classNames(
+                  selectedAppModuleId === module.id
+                    ? 'bg-csas-700 text-white'
+                    : 'text-csas-50 hover:text-white hover:bg-csas-700',
+                  'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold transition-all'
+                )}
+                on:click={() => {
+                  if (selectedAppModuleId !== module.id) selectedAppModule.set(module.id);
+                }}
+              >
+                <span class="truncate">{humanize(module.name)}</span>
+              </a>
+            </li>
+          {/each}
+        </ul>
+      </li>
     {/if}
     <li class="-mx-6 mt-auto">
       <p>nejaky vecicky dole hello</p>
