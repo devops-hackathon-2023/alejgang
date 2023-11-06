5c45f1f551fad9d6d552225b0e70712830f6b6d6
Jakub Srnka <jakub.srnka@volny.cz>
2023-11-06 23:51:51 +0000

    💄 humanize now does not capitalize

--- src/lib/string.ts
+++ src/lib/string.ts
@@ -1,8 +1,8 @@
 export const humanize = (phrase: string): string => {
-  return phrase.split('-').map(capitalize).join(' ');
+  return phrase.split('-').join(' ');
 };
 
-const capitalize = (word: string): string => {
+export const capitalize = (word: string): string => {
   return word[0].toUpperCase() + word.slice(1);
 };
 
