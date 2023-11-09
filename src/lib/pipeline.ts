99eb7a9e26fdfd11bddfb25d2a9beb4ea2fb521e
Ondřej Maxa <ondrej.maxa11@gmail.com>
2023-11-09 11:56:06 +0000

    ✨ running pipelines
    Signed-off-by: Ondřej Maxa <ondrej.maxa11@gmail.com>

--- src/lib/pipeline.ts
+++ src/lib/pipeline.ts
@@ -3,8 +3,8 @@ import { dateToStr, dateToUnix, strToDate, subYears } from '$lib/date';
 import { exec } from '$lib/io';
 import { weightedAverage } from '$lib/math';
 import { array, date, either, io, number, option, task, taskEither } from 'fp-ts';
-import type { Some } from 'fp-ts/lib/Option';
-import { min } from 'fp-ts/lib/Ord';
+import type { Option, Some } from 'fp-ts/lib/Option';
+import { max } from 'fp-ts/lib/Ord';
 import { pipe } from 'fp-ts/lib/function';
 
 type HasData = { duration: Some<number>; startedAt: Some<Date> };
@@ -44,5 +44,31 @@ export const remaining = (deployment: DeploymentResponse) =>
       ),
     ),
     taskEither.map(({ estimate, started }) => estimate - started),
-    taskEither.map((remaining) => min(number.Ord)(remaining, 0)),
+    taskEither.map((remaining) => max(number.Ord)(remaining, 0)),
+  );
+
+export const getRunning = (deploymentUnit: Option<string>) =>
+  pipe(
+    () =>
+      DeploymentsService.list3({
+        deploymentUnitId: deploymentUnit,
+        size: option.some(100),
+        startedAtLte: pipe(date.create, io.map(dateToStr), io.map(option.some), exec),
+        startedAtGte: pipe(date.create, subYears(1), io.map(dateToStr), io.map(option.some), exec),
+        status: option.some('STARTED'),
+      }),
+    taskEither.map(({ page }) => page),
+    taskEither.map(
+      array.map(($) =>
+        pipe(
+          $,
+          remaining,
+          taskEither.map((remainig) => ({
+            ...$,
+            remainig,
+          })),
+        ),
+      ),
+    ),
+    taskEither.flatMap(taskEither.sequenceArray),
   );
